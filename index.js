02bd758eb9153793df7782e4eec0cc57846dab7c
jeet1983 <sengupta.prosenjit@gmail.com>
2019-06-12 01:09:59 -0700

    STRY1701677: updated coments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 
 var app = express();
-//Respond with "hello world" for requests that hit our root "/" test
+//Respond with "hello world" for requests that hit our root "/" test test
 app.get('/', function (req, res) {
 res.send('hi world');
 });
