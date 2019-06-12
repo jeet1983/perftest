ac93d68288cd5543f1a82e99f211c022df604da7
jeet1983 <sengupta.prosenjit@gmail.com>
2019-06-12 00:42:47 -0700

    STRY1701677: updated test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var app = require('../index.js');
 describe('Test Get Call', function() {
  it('Verify if response is hello world', function(done) {
   //navigate to root and check the response is "hello world"
-  request(app).get('/').expect('hello world', done);
+  request(app).get('/').expect('hi world', done);
  });
 });
